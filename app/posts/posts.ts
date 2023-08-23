5a2740305d4a968b8d8615f586f480d561c9b398
22 <60903333+nini22P@users.noreply.github.com>
2023-08-23 18:51:59 +0800

    Add remark-gfm

--- app/posts/posts.ts
+++ app/posts/posts.ts
@@ -3,6 +3,7 @@ import path from 'path'
 import matter from 'gray-matter'
 import { remark } from 'remark'
 import html from 'remark-html'
+import remarkGfm from 'remark-gfm'
 
 const postsDirectory = path.join(process.cwd(), 'posts')
 
@@ -41,6 +42,7 @@ export async function getPostData(slug: string) {
 
   const processedContent = await remark()
     .use(html)
+    .use(remarkGfm)
     .process(matterResult.content)
   const contentHtml = processedContent.toString()
 
