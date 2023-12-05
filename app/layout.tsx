675a8d175f771f43978ef0f4925b743ca1592566
22 <60903333+nini22P@users.noreply.github.com>
2023-12-05 12:34:40 +0800

    Add clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { Metadata } from 'next'
 import './globals.css'
 import styles from './layout.module.css'
-import 'material-icons/iconfont/material-icons.css'
 import siteConfig from '../site.config'
 import Navigation from '../components/Navigation/Navigation'
+import Script from 'next/script'
 // import Status from '../components/Status/Status'
 // import Dock from '../components/Dock/Dock'
 
@@ -17,6 +17,17 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <html>
+
+      <Script strategy="lazyOnload" id="clarity-script">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "k0zldg2mow");
+        `}
+      </Script>
+
       <body>
         <div className='w-full min-h-screen pb-20 px-4 pt-4 xl:pt-8'>
           <div className='max-w-7xl m-auto'>
