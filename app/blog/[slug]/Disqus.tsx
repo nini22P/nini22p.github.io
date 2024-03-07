d32ae3684e13559f10170e07a15132267901d6a7
22 <60903333+nini22P@users.noreply.github.com>
2024-03-07 15:52:21 +0800

    Add Disqus

---
+++ app/blog/[slug]/Disqus.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { DiscussionEmbed } from "disqus-react"
+
+const Disqus = ({ shortname, url, identifier, title }: { shortname: string, url: string, identifier: string, title: string }) => {
+  return (
+    <DiscussionEmbed
+      shortname={shortname}
+      config={
+        {
+          url: url,
+          identifier: identifier,
+          title: title,
+        }
+      }
+    />
+  )
+}
+
+export default Disqus
--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,6 +3,8 @@ import Date from '../../../components/Date'
 import { getPostList, getPost } from '../posts'
 import Card from '../../../components/Card/Card'
 import siteConfig from '../../../site.config'
+import { DiscussionEmbed } from 'disqus-react'
+import Disqus from './Disqus'
 
 export const generateMetadata = async ({ params: { slug } }: { params: { slug: string } }) => {
   const post = await getPost(slug)
@@ -22,13 +24,19 @@ const Post = async ({ params: { slug } }: { params: { slug: string } }) => {
 
   return (
     <Card className='max-w-4xl m-auto overflow-x-hidden p-4 pt-8 rounded-lg'>
-      <article className='prose prose-slate max-w-4xl'>
+      <article className='prose prose-slate max-w-4xl mb-8'>
         <h1>{post.data.title}</h1>
         <div>
           <Date dateString={post.data.date} />
         </div>
         <div dangerouslySetInnerHTML={{ __html: post.contentHtml }} />
       </article>
+      <Disqus
+        shortname='nini22p'
+        url={`${siteConfig.url}/blog/${post.slug}`}
+        identifier={post.slug}
+        title={post.data.title}
+      />
     </Card>
   )
 }
