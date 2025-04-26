64ce246376ac8a56d6be31414000f86a0aa6105f
22 <60903333+nini22P@users.noreply.github.com>
2025-04-26 19:00:54 +0800

    update data

--- data/apps.ts
+++ data/apps.ts
@@ -1,7 +1,9 @@
 const apps: { title: string, path?: string, url?: string, }[] = [
   { title: '博客', path: '/blog' },
+  { title: 'Iris', url: 'https://github.com/nini22P/Iris' },
   { title: 'OMP', url: 'https://nini22p.github.io/omp' },
-  { title: 'NPS 列表', url: 'https://nini22p.github.io/nps-list/' },
+  { title: 'Clash Composer', url: 'https://github.com/nini22P/clash-composer' },
+  { title: 'FNPS', url: 'https://github.com/nini22P/fnps' },
   { title: 'WebGAL Android', url: 'https://github.com/nini22P/WebGAL-Android' },
   { title: 'GOG 临时改区工具', url: 'https://github.com/nini22P/gog-lc-tool' },
   { title: '密码生成器', url: 'https://github.com/nini22P/password-generator' },
--- data/home.ts
+++ data/home.ts
@@ -1,5 +1,6 @@
 const home: { title: string, size: { col: number, row: number }, path?: string, url?: string, }[] = [
   { title: '博客', size: { col: 2, row: 1 }, path: '/blog' },
+  { title: 'Iris', size: { col: 1, row: 1 }, url: 'https://github.com/nini22P/Iris' },
   { title: 'OMP', size: { col: 1, row: 1 }, url: 'https://nini22p.github.io/omp' },
 ]
 
